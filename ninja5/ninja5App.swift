50112d4699b1db390d69ffc6cd06500ebfbb13c6
lukasg8 <102938476+lukasg8@users.noreply.github.com>
2023-04-26 17:39:21 -0500

    Initial Commit

---
+++ ninja5/ninja5App.swift
@@ -0,0 +1,17 @@
+//
+//  ninja5App.swift
+//  ninja5
+//
+//  Created by Lukas on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ninja5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
