e277e27b7c557e0256e67c9cea725b6c509d9c1a
achiapparoli <achiapparoli@uchicago.edu>
2023-05-07 11:28:37 -0500

    lol

--- ninja5/Views/NoteRowView.swift
+++ ninja5/Views/NoteRowView.swift
@@ -2,30 +2,6 @@
 //  NoteRowView.swift
 //  ninja5
 //
-//  Created by Lukas on 5/3/23.
+//  Created by Alberto Chiapparoli on 07/05/23.
 //
 
-import SwiftUI
-
-struct NoteRowView: View {
-    
-    let note: Note
-    
-    var body: some View {
-        VStack {
-            HStack {
-                Text(note.title)
-            }
-            Divider()
-                .padding(.vertical, 5)
-        }
-    }
-}
-
-struct NoteRowView_Previews: PreviewProvider {
-
-    static let note = Note(id: UUID(), canvasData: Data(), title: "Homework 1", selected: false)
-    static var previews: some View {
-        NoteRowView(note:note)
-    }
-}
